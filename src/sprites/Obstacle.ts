e3f6a5a1a87535951324cbb96d8534368587ade5
huytran19 <tranhoanghuy119@gmail.com>
2021-11-30 18:43:32 +0700

    Second Commit

---
+++ src/sprites/Obstacle.ts
@@ -0,0 +1,73 @@
+import { Vector } from "@/types";
+import { CanvasView } from "@/view/CanvasView";
+
+
+import FLY_DOWN_IMAGE from '../images/bird_down.png'
+import FLY_UP_IMAGE from '../images/bird_up.png'
+
+export class Obstacle {
+    private obstacleImage: HTMLImageElement = new Image()
+    private obstacleSpeed: number
+    private flyTime: number
+    private deltaTime: number
+    private isFlyDown: boolean
+
+    constructor(
+        private obstacleWidth: number,
+        private obstacleHeight: number,
+        private position: Vector,
+        image: string,
+        speed: number
+    ) {
+        this.obstacleWidth = obstacleWidth
+        this.obstacleHeight = obstacleHeight
+        this.position = position
+        this.obstacleImage.src = image
+        this.obstacleSpeed = speed
+        this.flyTime = 0
+        this.deltaTime = 0.0625
+        this.isFlyDown = false
+    }
+
+    get width(): number {
+        return this.obstacleWidth
+    }
+
+    get height(): number {
+        return this.obstacleHeight
+    }
+
+    get pos(): Vector {
+        return this.position
+    }
+
+    get image(): HTMLImageElement {
+        return this.obstacleImage
+    }
+
+    Run(type: string): void {
+        this.flyTime += this.deltaTime
+        if (type == "cactus") this.position.x -= this.obstacleSpeed
+        
+        if (type == "bird") {
+            if (this.flyTime > 1) {
+                this.flyTime = 0
+                this.Fly()
+            }
+            
+        }
+    }
+
+    Fly(): void {
+        if (this.isFlyDown) {
+            this.obstacleImage.src = FLY_UP_IMAGE
+            this.isFlyDown = false
+            return
+        }
+        if (!this.isFlyDown) {
+            this.obstacleImage.src = FLY_DOWN_IMAGE
+            this.isFlyDown = true
+            return
+        }
+    }
+}
---
+++ src/view/CanvasView.ts
@@ -0,0 +1,60 @@
+import { Vector } from "@/types";
+import { Player, Obstacle, Background } from "@/sprites";
+export class CanvasView {
+    canvas: HTMLCanvasElement
+    private context: CanvasRenderingContext2D | null
+    private scoreDisplay: HTMLObjectElement | null
+    private start: HTMLObjectElement | null
+    private info: HTMLObjectElement | null
+
+    constructor(canvasName: string) {
+        this.canvas = document.querySelector(canvasName) as HTMLCanvasElement
+        this.context = this.canvas.getContext('2d')
+        this.scoreDisplay = document.querySelector('#score')
+        this.start = document.querySelector('#start')
+        this.info = document.querySelector('#info')
+    }
+
+    clear(): void {
+        this.context?.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    }
+
+    initStartButton(startFunction: (view: CanvasView) => void): void {
+        this.start?.addEventListener('click', () => startFunction(this))
+    }
+
+    drawScore(score: number): void {
+        if (this.scoreDisplay) this.scoreDisplay.innerHTML = "Score: " + score.toString()
+    }
+
+    drawInfo(highScore: number): void {
+        if (this.info) this.info.innerHTML = "High score: " + highScore.toString()
+    }
+
+    drawSprite(entity: Player | Obstacle | Background): void {
+        if (!entity) return
+        this.context?.drawImage(
+            entity.image,
+            entity.pos.x,
+            entity.pos.y,
+            entity.width,
+            entity.height,
+        )
+    }
+
+    drawBackground(entity: Background): void {
+        if (!entity) return
+        
+        this.context?.drawImage(
+            entity.image,
+            entity.pos.x + entity.width,
+            entity.pos.y,
+            entity.width,
+            entity.height,
+        )
+    }
+
+    drawObstacles(obstacles: Obstacle[]): void {
+        obstacles.forEach(obstacle => this.drawSprite(obstacle))
+    }
+}
